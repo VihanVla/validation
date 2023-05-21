a217d44e011c643f0db5335d16465a40c199a4a7
VihanVla <111562310+VihanVla@users.noreply.github.com>
2023-05-21 17:24:00 +0330

    adding username regex

--- validation.js
+++ validation.js
@@ -2,6 +2,8 @@ let form     = document.querySelector(".form");
 let username = document.querySelector("#username")
 let password = document.querySelector("#pasword")
 let seepassword = document.querySelector("#see-pasword")
+// userpattern => up
+let up = /^[a-zA-Z][\w_.]{6,24}$/
 
 form.addEventListener("submit", e=>{
     e.preventDefault()  
@@ -9,7 +11,15 @@ form.addEventListener("submit", e=>{
 
 form.username.addEventListener("keyup", e=>{
     if (e.target.value){
-    username.textContent = e.target.value
+        username.textContent = e.target.value
+        if (up.test(e.target.value)) {
+
+            e.target.classList.add("is-valid")
+            e.target.classList.remove("is-invalid")
+        }
+        else {
+            e.target.classList.add("is-invalid")
+        }
 }
 else{
     username.textContent = "please enter your username"
